70b611d55947b918f41b2942e43e38585e163f09
toucansam <gumbyclownshoes@gmail.com>
2019-04-16 15:54:29 -0500

    Updated runlog to search for pm2 path

--- packages/plugin-monitor/src/runLog.ts
+++ packages/plugin-monitor/src/runLog.ts
@@ -1,11 +1,39 @@
 import { spawn } from 'child_process'
+import { exists as rawExists } from 'fs'
 import { resolve } from 'path'
+import { promisify } from 'util'
+
+const exists = promisify(rawExists)
 
 
-export default function runLogs(processName: string, lines: number | undefined) {
-  const pm2Path = resolve(__dirname, '..', 'node_modules', 'pm2', 'bin', 'pm2')
+function runPm2LogsCommand(pm2path: string, processName: string, lines: number | undefined) {
   const commandArgs = ['logs', processName]
   const flagArgs = lines ? ['--lines', lines.toString()] : []
 
-  spawn(pm2Path, commandArgs.concat(flagArgs), { stdio: 'inherit' })
+  spawn(pm2path, commandArgs.concat(flagArgs), { stdio: 'inherit' })
+}
+
+export default async function runLogs(processName: string, lines: number | undefined) {
+  return new Promise(async (success, reject) => {
+    try {
+      const localPm2Path = resolve(__dirname, '..', 'node_modules', 'pm2', 'bin', 'pm2')
+      const localPm2Exists = await exists(localPm2Path)
+      if (localPm2Exists) {
+        await runPm2LogsCommand(localPm2Path, processName, lines)
+        return success()
+      }
+
+      const topLevelPm2Path = resolve(__dirname, '..', '..', '..', 'pm2', 'bin', 'pm2')
+      const topLevelPm2Exists = await exists(topLevelPm2Path)
+      if (topLevelPm2Exists) {
+        await runPm2LogsCommand(topLevelPm2Path, processName, lines)
+        return success()
+      }
+
+      reject(new Error(`Could not find module pm2 installed.`))
+    }
+    catch (ex) {
+      reject (ex)
+    }
+  })
 }
