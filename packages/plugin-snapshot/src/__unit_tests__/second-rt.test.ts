162802bd9e47f63ecd969c74c81274fbe6db9f21
toucansam <gumbyclownshoes@gmail.com>
2019-05-02 18:22:19 -0500

    Added fake test for plugin-snapshot to test ci

---
+++ packages/plugin-snapshot/src/__unit_tests__/second-rt.test.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-floating-promises */
+import { describe } from 'riteway'
+
+describe('snapshot', async assert => {
+  assert({
+    given: '2nd test',
+    should: 'be successful',
+    actual: 1,
+    expected: 1,
+  })
+})
